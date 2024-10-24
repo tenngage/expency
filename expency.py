ea458b0db1ee92caa7037210d986971307ab1d74
tenngage <ibi0880@gmail.com>
2024-10-24 17:38:03 +0300

    get_query logic

--- expency.py
+++ expency.py
@@ -11,11 +11,12 @@ def main():
 
     supported_queries = get_supported_queries()
 
-    get_query(supported_queries)
+    args, query = get_query(supported_queries)
+    print(args)
+    print(query)
+    
 
     save_database(DATABASE_PATH, database)
-
-
 
 def load_database(DATABASE_PATH: str) -> dict:
     try:
@@ -30,10 +31,65 @@ def save_database(DATABASE_PATH: str, database: dict) -> None:
         json.dump(database, f, indent=4)
 
 def get_supported_queries() -> dict:
-    pass
+    return {
+        "add": {
+            "target": add,
+            "name_or_flags": [{
+                "argument": "name",
+                "help": "Name of an expense"
+            },
+            {
+                "argument": "amount",
+                "help": "Amount of an expense"
+            }],
+            "help": "Add your expense"
+        },
+        "update": {
+            "target": update,
+            "name_or_flags": [{
+                "argument": "id",
+                "help": "ID of an expense you want to update"
+            },
+            {
+                "argument": "new_name",
+                "help": "Updated name of an expense"
+            },
+            {
+                "argument": "new_amount",
+                "help": "Updated amount of an expense"
+            }],
+            "help": "Update the expense by it's id"  
+        },
+        "delete": {
+            "target": delete,
+            "name_or_flags": [{
+                "argument": "id",
+                "help": "ID of an expense you want to delete"
+            }],
+            "help": "Delete the expense by it's id"
+        },
+        "list": {
+            "target": list_expances,
+            "name_or_flags": [{
+                "argument": "display",
+                "help": "Specify what you want to display"
+            }],
+            "help": "List all expenses"
+        }
+    }
 
 def get_query(supported_queries: dict):
-    pass
+    parser = ArgumentParser(description="CLI application to manage your expenses")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    for command, args in supported_queries.items():
+        new_parser = subparsers.add_parser(command, help=args["help"])
+        for arg in args["name_or_flags"]:
+            new_parser.add_argument(arg["argument"], help=arg["help"])
+
+    args = parser.parse_args().__dict__
+    query = supported_queries[args["command"]]["target"]
+    return args, query
 
 def add():
     pass
