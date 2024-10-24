8d3bafa765cb761bd3ebe39aa2e69b1e26b05f22
tenngage <ibi0880@gmail.com>
2024-10-24 13:47:58 +0300

    App structure

---
+++ expency.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from tabulate import tabulate
+
+def main():
+    pass
+
+def get_supported_queries():
+    pass
+
+def get_query():
+    pass
+
+def add():
+    pass
+
+def update():
+    pass
+
+def delete():
+    pass
+
+def list_expances():
+    pass
+
+if __name__ == "__main__":
+    main()
