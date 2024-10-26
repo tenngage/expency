30f82d41de44f3396e71d786180f27a4a4179d8a
tenngage <ibi0880@gmail.com>
2024-10-26 15:59:00 +0300

    Missing things

--- expency.py
+++ expency.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 from tabulate import tabulate
+from typing import Callable, Generator, Tuple
+from datetime import datetime
 import os
 import json
 
@@ -9,13 +11,12 @@ def main():
 
     database: dict = load_database(DATABASE_PATH)
 
-    supported_queries = get_supported_queries()
+    supported_queries: dict = get_supported_queries()
 
     args, query = get_query(supported_queries)
-    print(args)
-    print(query)
-    
 
+    query(database, **args)
+    
     save_database(DATABASE_PATH, database)
 
 def load_database(DATABASE_PATH: str) -> dict:
@@ -35,11 +36,13 @@ def get_supported_queries() -> dict:
         "add": {
             "target": add,
             "name_or_flags": [{
-                "argument": "name",
+                "argument": ["--name", "-n"],
+                "required": True,
                 "help": "Name of an expense"
             },
             {
-                "argument": "amount",
+                "argument": ["--amount", "-a"],
+                "required": True,
                 "help": "Amount of an expense"
             }],
             "help": "Add your expense"
@@ -47,15 +50,18 @@ def get_supported_queries() -> dict:
         "update": {
             "target": update,
             "name_or_flags": [{
-                "argument": "id",
+                "argument": ["--id", "-i"],
+                "required": True,
                 "help": "ID of an expense you want to update"
             },
             {
-                "argument": "new_name",
+                "argument": ["--new-name"],
+                "required": False,
                 "help": "Updated name of an expense"
             },
             {
-                "argument": "new_amount",
+                "argument": ["--new-amount"],
+                "required": False,
                 "help": "Updated amount of an expense"
             }],
             "help": "Update the expense by it's id"  
@@ -63,45 +69,65 @@ def get_supported_queries() -> dict:
         "delete": {
             "target": delete,
             "name_or_flags": [{
-                "argument": "id",
+                "argument": ["--id", "-i"],
+                "required": True,
                 "help": "ID of an expense you want to delete"
             }],
             "help": "Delete the expense by it's id"
         },
         "list": {
-            "target": list_expances,
+            "target": list_expenses,
             "name_or_flags": [{
-                "argument": "display",
-                "help": "Specify what you want to display"
+                "argument": ["--month", "-m"],
+                "required": False,
+                "help": "Choose month to display specified expenses"
             }],
             "help": "List all expenses"
         }
     }
 
-def get_query(supported_queries: dict):
-    parser = ArgumentParser(description="CLI application to manage your expenses")
+def get_query(supported_queries: dict) -> Tuple[dict, Callable[..., None]]:
+    parser: ArgumentParser = ArgumentParser(description="CLI application to manage your expenses")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     for command, args in supported_queries.items():
         new_parser = subparsers.add_parser(command, help=args["help"])
         for arg in args["name_or_flags"]:
-            new_parser.add_argument(arg["argument"], help=arg["help"])
+            new_parser.add_argument(*arg["argument"], required=arg["required"], help=arg["help"])
 
-    args = parser.parse_args().__dict__
-    query = supported_queries[args["command"]]["target"]
+    args: dict = parser.parse_args().__dict__
+    query: Callable[..., None] = supported_queries[args.pop("command")]["target"]
     return args, query
 
-def add():
-    pass
+def add(database: dict, name: str, amount: str) -> None:
+    today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    id: str = str(max(0, len(database.keys())) + 1)
+    database[id] = {
+        "name": name,
+        "amount": amount,
+        "created at": today
+    }
 
-def update():
-    pass
+def update(database: dict, id: str, new_name: str, new_amount: str) -> None:
+    database[id]["name"] = new_name
+    database[id]["amount"] = new_amount
 
-def delete():
-    pass
+def delete(database: dict, id: str) -> None:
+    del database[id]
 
-def list_expances():
-    pass
+def list_expenses(database: dict, month: str) -> None:
+    if month is None:
+        month = "all"
+    table: Generator = ({
+        "id": index,
+        "name": properties["name"],
+        "amount": properties["amount"],
+        "created at": properties["created at"]
+    }
+        for index, properties in database.items()
+        if month == "all" or month == database[index]["created at"].split("-")[1]
+    )
+    print(tabulate(table, headers="keys", tablefmt="rounded_outline"))
 
 if __name__ == "__main__":
     main()
